bd7b2f3243a3361ef1415f498c636234710091cc
CvGemert <caspervangemert@gmail.com>
2019-12-01 19:01:10 +0100

    Refactoring

--- src/store/en/index.js
+++ src/store/en/index.js
@@ -1,6 +1,5 @@
 export default {
   state: {
-    selectedMethod: '',
     adding: [
       {
         name: 'splice',
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import vuexI18n from 'vuex-i18n'
+
+// import languages (only state translations)
+import en from './en/index'
+import translationsEn from '../locale/en/index'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const mutations = {
+  selectionMethod(state, selected) {
+    state.selectedMethod = selected
   },
-  mutations: {
+  resetSelection(state) {
+    state.selectedMethod = ''
   },
-  actions: {
-  },
+}
+
+export const store = new Vuex.Store({
   modules: {
-  }
+    en,
+  },
+  state: {
+    selectedMethod: '',
+    curLanguage: 'en',
+  },
+  mutations
 })
+
+Vue.use(vuexI18n.plugin, store)
+
+// add translations directly to the application
+Vue.i18n.add('en', translationsEn)
+
+Vue.i18n.set(store.state.curLanguage)
+
+// registering only the current language
+// would be also possible but unloading & loading would be required
+// at language change --> can be added later (for now load all langauges)
+// store.registerModule('de', de);
